2e398748b011e7b65d0000425de6f1c7aa6b4704
Willi Mentzel <willi.mentzel@gmail.com>
2017-01-19 21:54:49 +0100

    added button to MainActivity

--- app/src/main/java/com/randomlychosenbytes/myapplication/MainActivity.java
+++ app/src/main/java/com/randomlychosenbytes/myapplication/MainActivity.java
@@ -12,6 +12,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -28,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void onOpenFileChooserDialogButton(View view){
         getPrivileges();
     }
 
@@ -40,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFileSelected(final File file) {
                 Log.i("info", file.getAbsolutePath());
+                ((TextView) findViewById(R.id.textView)).setText(file.getAbsolutePath());
             }
         });
 
